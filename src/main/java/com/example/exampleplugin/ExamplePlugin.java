fface98ceee6ebadaff653917a693fb3a89ab19f
Daniel Fanara <fanardat16@gmail.com>
2016-01-16 22:06:23 -0500

    Initial Push

---
+++ src/main/java/com/example/exampleplugin/ExamplePlugin.java
@@ -0,0 +1,17 @@
+package com.example.exampleplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ExamplePlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {	
+
+	}
+
+	@Override
+	public void onDisable() {	
+
+	}
+
+}
